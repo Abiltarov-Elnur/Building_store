bea5af42da65b2dffca8cd1199257257d0b2f962
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-26 23:41:14 +0300

    Последняя успешная работа сайта, появилась кнопка перехода на товары

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>🏠 Главная страница</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
       </Routes>
